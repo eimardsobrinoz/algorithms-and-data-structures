64f1dd47a12e088d5238f0cc79c8db0d7f64d889
esobrinoz <eimard_11@hotmail.com>
2021-06-08 01:35:01 +0200

    Challenge

--- CHALLENGES/uniqueCharacters.ts
+++ CHALLENGES/uniqueCharacters.ts
@@ -4,7 +4,7 @@
 * 
 * Questions or Clarifications to take into account
 * 
-*  Is the string in ASCII or Unicode ? --> ASCII (which are 127 characters)
+*  Is the string in ASCII or Unicode ? --> ASCII (which are 128 characters)
 *    If not, we would need to increase the storage size
 *
 *  Can I assume strings come as lowecase? 
@@ -15,7 +15,7 @@
 *      - Make use of Set
 *      - Time Complexity: O(n) where n is the length of the string.
 *      - Space Complexity: O(1)
-*        Anyway, since the for loop is not going to be iterate through more than 127 characters that could be
+*        Anyway, since the for loop is not going to be iterate through more than 128 characters that could be
 *        considered as constat O(1)
 *       If we do not assume that the set is fixed, we could express the complexity of this algorithm as:
 *         Time complexity: O(min(c,n)) where n is the lenght of the string and c the size of the character set.
@@ -28,7 +28,7 @@
 
 function isUniqueChars(word: string): boolean {
   let length: string = word.length;
-  if (length > 127) return false;
+  if (length > 128) return false;
   const char_set: Set<string> = new Set<string>();
   for(let i = length - 1; i >= 0; i--) {
     const element: string = word.charAt(i);
